6cec6c63579d4a849a0efc5b663a3fb7f36c1eea
samson wong <steelerwong2005@hotmail.com>
2020-09-29 02:37:08 -0400

    Updates

---
+++ precache-manifest.8445af71278856cf4fa0c5ca299b1ad9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7c10fd74a0b1296dac178eaf21d23b6",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "226d2fa88e89876d560a",
+    "url": "/ReactPortfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "b01325a9b11e03a69243",
+    "url": "/ReactPortfolio/static/css/main.1f7d458d.chunk.css"
+  },
+  {
+    "revision": "226d2fa88e89876d560a",
+    "url": "/ReactPortfolio/static/js/2.2e625d98.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactPortfolio/static/js/2.2e625d98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b01325a9b11e03a69243",
+    "url": "/ReactPortfolio/static/js/main.4e271431.chunk.js"
+  },
+  {
+    "revision": "0114f5eceb8464d5d956",
+    "url": "/ReactPortfolio/static/js/runtime-main.5b3ce1ae.js"
+  },
+  {
+    "revision": "2d14bacf8eb49a673286a3ebcf88ce99",
+    "url": "/ReactPortfolio/static/media/BurgerMVC.2d14bacf.PNG"
+  },
+  {
+    "revision": "edaaecb3d87d38c9bec5c81606145456",
+    "url": "/ReactPortfolio/static/media/Project1FoodSearch.edaaecb3.PNG"
+  },
+  {
+    "revision": "c16e17785fd2e70bb1ac75e498fbd05e",
+    "url": "/ReactPortfolio/static/media/Project2InventorySystem.c16e1778.PNG"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/ReactPortfolio/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "bd80794fddbffb4031fab537cc898680",
+    "url": "/ReactPortfolio/static/media/myImage.bd80794f.png"
+  },
+  {
+    "revision": "edca20ae54b2d75b1e2a88846eeb393f",
+    "url": "/ReactPortfolio/static/media/wk3PasswordGenerator.edca20ae.PNG"
+  },
+  {
+    "revision": "a34685ed169f5fa6ef115f7ccaa910ba",
+    "url": "/ReactPortfolio/static/media/wk5WorkDay.a34685ed.PNG"
+  },
+  {
+    "revision": "c381f9421d38cb9eef56f267b50f28bf",
+    "url": "/ReactPortfolio/static/media/wk6WeatherDashboard.c381f942.PNG"
+  }
+]);
